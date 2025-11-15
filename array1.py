c35ccd3f83a41bcdde0128d69ec471ddadfbdf20
sindhuhegde2007-eng <sindhuhegde2007@gmail.com>
2025-11-15 13:31:45 +0530

    save

--- array1.py
+++ array1.py
@@ -1,9 +1,15 @@
 import sys
 
+# Default numbers already included
+scores = [10, 20, 30, 40, 50]
+
 if len(sys.argv) > 1:
     scores = [float(n) for n in sys.argv[1:]]
 else:
-    scores = [float(n) for n in input("Enter scores: ").split()]
+    # If you don’t type anything, these numbers will be used
+    print("Using default scores:",scores)
+   
+
 total = sum(scores)
 average = total / len(scores)
 maximum = max(scores)
